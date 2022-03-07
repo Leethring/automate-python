b385efd8e6d452692a1d543cabcc764ab416382e
Li Weiwei <15216601917@163.com>
2022-03-07 11:39:33 +0800

    Write original quiz file

--- project/randomQuizGenerator/randomQuizGenerator.py
+++ project/randomQuizGenerator/randomQuizGenerator.py
@@ -1,6 +1,7 @@
 #! python3
 # randomQuizgenerator.py - Creates quizzes with questions and answer 
 # in random order, along with the answer key.
+# 
 # The Quiz library have 50 options
 # For every student, create 50 questions from the quiz library with 35 
 # different quiz files.
@@ -8,6 +9,7 @@
 
 import random 
 
+# Existed capitals dictionary
 capitals = {
     'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 
     'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
@@ -28,37 +30,40 @@ capitals = {
     }
 
 # create 35 quiz files from capitals in capitalquiz.txt file
-# create related answers in another file
+# create related answers in capitalsquiz_answers.txt file
 for quizNum in range(35):
     # Create the quiz and answer key files.
-        # create 35 capitalsquiz.txt with different affix numbers, such capitalsquiz1.txt
-        # capitalsquiz2.txt
+
+    # Create 35 capitalsquiz.txt with different affix numbers, such capitalsquiz1.txt
+    # Capitalsquiz2.txt, capitalsquiz2.txt
     quizFile = open('capitalsquiz%s.txt' % (quizNum + 1), 'w') 
-        # create 35 capitalsquiz_answers.txt with affix numbers
+    # Create 35 capitalsquiz_answers.txt with affix numbers
     answerKeyFile = open('capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
 
     # Write out the header for the quiz.
-        # write some line in every quizFile
-        # Name:
-        # 
-        #
-        # Date:
-        #
-        #
-        # Period:
-        #
-        #
+
+    # Write some lines in every quizFile
+    # Name:
+    # 
+    #
+    # Date:
+    #
+    #
+    # Period:
+    #
+    #
     quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
-        # Write in the middle with quiz number
-        #                       State Capitals Quiz... 
+    # Write in the middle with quiz number
+    #                       State Capitals Quiz... 
     quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
     quizFile.write('\n\n')
 
     # Shuffle the order of the states:
-        # Give the keys of the capitals dictionary to states in 
-        # a form of list
+
+    # Give the keys of the capitals dictionary to states in 
+    # a form of list
     states = list(capitals.keys()) 
-        # shuffle the order of states lists
+    # Shuffle the order of states lists
     random.shuffle(states) 
 
 
@@ -67,33 +72,36 @@ for quizNum in range(35):
         # Get right and wrong answer. 
         # Have shuffled states list
         # Create 50 questions for this quiz file
-            # For questionNum, create a correct answer 
-            # Use corresponded keys to get the values from capitals
+
+        # For questionNum, create a correct answer 
+        # Use corresponded keys to get the values from capitals
         correctAnswer = capitals[states[questionNum]]
-            # Give all values go the wrongAnswers
+        # Give all values go the wrongAnswers
         wrongAnswers = list(capitals.values())
-            # Delete the correct answer from wrong answers list
-            # By using index of correct answer
+        # Delete the correct answer from wrong answers list
+        # By using index of correct answer
         del wrongAnswers[wrongAnswers.index(correctAnswer)]
-            # Assignment random 3 elements from the wrong answers list
+        # Assign random 3 elements from the wrong answers list
         wrongAnswers = random.sample(wrongAnswers, 3)
-            # Assignment 4 options to the answerOptions 
+        # Assign 4 options to the answerOptions 
         answerOptions = wrongAnswers + [correctAnswer]
-            # shuffle the answerOptions
+        # Shuffle the answerOptions
         random.shuffle(answerOptions)
 
         # Write the question and answer options to the quiz file
-            # Create question model with listed number
+
+        # Create question model with listed number
         quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
         for i in range(4):
             # Create ABCD four options by using i 
-            quizFile.write('    %s. %s\n' % ('ABCD'[i], answerOptions[i]))
-        quizFile.write('\n')
+            quizFile.write(' %s. %s\n' % ('ABCD'[i], answerOptions[i]))
 
         # Write the answer key to a file
-            # Give every number the correct answer. 
+        # Give every number the correct answer. 
         answerKeyFile.write('%s. %s\n' % (questionNum + 1, 
         'ABCD'[answerOptions.index(correctAnswer)]))
 
+    # Close opened file for each quiz loop in the end
+    # to avoid error
     quizFile.close()
     answerKeyFile.close()
